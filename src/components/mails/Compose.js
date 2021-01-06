abd1fd5bb0c57385039521bf5bf06d57a58aa052
bodroghykristof <bodroghykristof@gmail.com>
2021-01-06 15:41:34 +0100

    feat: implement fetching all users from server

--- src/components/mails/Compose.js
+++ src/components/mails/Compose.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import backEnd from '../general/Backend';
 
 function Compose() {
-    return (
-        <div>
-            Compose
-        </div>
-    )
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    async function getUsers() {
+      const receivedMails = await axios.get(`${backEnd.address}/api/users`);
+      setUsers(receivedMails.data);
+    }
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <input type='text' placeholder='Subject' />
+      <input type='text' placeholder='Subject' />
+      <br></br>
+      <textarea
+        name='message'
+        id=''
+        cols='30'
+        rows='10'
+        placeholder='Type here...'
+      ></textarea>
+      <br></br>
+      <button>Send</button>
+    </div>
+  );
 }
 
-export default Compose
+export default Compose;
