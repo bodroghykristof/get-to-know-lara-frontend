2de1f266932acc5fb774f4fb7f4b102099ddd36e
bodroghykristof <bodroghykristof@gmail.com>
2021-01-06 13:56:12 +0100

    feat: implement fetching and displaying emails

--- src/components/mails/Inbox.js
+++ src/components/mails/Inbox.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import backEnd from '../general/Backend';
 
 function Inbox() {
-  return <div>My Inbox</div>;
+  const userId = 1;
+  const [mails, setMails] = useState([]);
+
+  useEffect(() => {
+    async function getMails() {
+      const receivedMails = await axios.get(
+        `${backEnd.address}/api/mails/to/${userId}`
+      );
+      setMails(receivedMails.data);
+    }
+    getMails();
+  }, []);
+
+  if (mails === null) return;
+
+  return (
+    <div>
+      {mails.map((mail, index) => (
+        <p key={index}>{mail.message}</p>
+      ))}
+    </div>
+  );
 }
 
 export default Inbox;
