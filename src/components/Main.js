8b370c887df478c817f5780049ad4e9fbf174321
bodroghykristof <bodroghykristof@gmail.com>
2021-01-11 20:57:14 +0100

    refactor: remove unused imports

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import backEnd from './general/Backend';
 
 function Main() {
   return (
